e23e551db00e401440ccb09cd1be767a0ec4adb7
Nemo <me@captnemo.in>
2019-02-18 01:09:19 +0530

    Change OpensslBasics -> Setup

--- koans/SetupKoans.php
+++ koans/SetupKoans.php
@@ -6,15 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 defined('__') or define('__', null);
 
-class OpensslBasicKoans extends TestCase {
+class SetupKoans extends TestCase {
     public function testEnvironmentIsZen() {
         $this->assertNotNull(`which openssl 2>/dev/null`);
+        $this->assertNotNull(`which docker 2>/dev/null`);
         $this->assertTrue(count(array_intersect(get_loaded_extensions(), ['curl', 'openssl'])) == 2);
     }
 
     public function testOpensslVersion() {
         // Change Me
-        $command = "version";
+        $command = "changeme";
 
         exec("openssl $command 2>&1", $output);
 
