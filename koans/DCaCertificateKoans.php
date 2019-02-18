cede9de25e4f0a035b18f02790b0b384cab9f9bf
Nemo <me@captnemo.in>
2019-02-18 16:46:09 +0530

    Switch test to CA certificate

--- koans/DCaCertificateKoans.php
+++ koans/DCaCertificateKoans.php
@@ -9,14 +9,17 @@ class DCaCertificateKoans extends TestCase {
      * !Generate a self-signed certificate in files/6.pem
      *  And the corresponding private key (with no passphrase) in files/6.key
      *  Use common name as crypto.koans.invalid
+     *
+     * If generated with defaults, it will automatically be a CA certificate
      */
-    public function testSelfSignedCertificateExists() {
-        $key = openssl_pkey_get_private('file://files/6.key');
-        $cert = openssl_x509_read(file_get_contents('files/6.pem'));
+    public function testCaCertificateExists() {
+        $key = openssl_pkey_get_private('file://files/ca.key');
+        $cert = openssl_x509_read(file_get_contents('files/ca.pem'));
 
         $info = openssl_x509_parse($cert);
 
         $this->assertEquals('crypto.koans.invalid', $info['subject']['CN']);
+        $this->assertEquals('CA:TRUE', $info['extensions']['basicConstraints']);
         $this->assertTrue(
             openssl_x509_check_private_key($cert, $key)
         );
